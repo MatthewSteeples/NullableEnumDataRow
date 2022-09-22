b09e53f3aabf52b9448535a5a284be425ae86195
Matthew Steeples <matthew@ledgerscope.com>
2022-09-22 15:41:41 +0100

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NullableEnumDataRow
+{
+	public enum CountryCode
+	{
+		NL = 1,
+		GB = 2,
+		FR = 3
+	}
+
+	[TestClass]
+	public class UnitTests
+	{
+		[DataTestMethod]
+		[DataRow("NL", CountryCode.NL)]
+		[DataRow("GB", CountryCode.GB)]
+		[DataRow("FR", CountryCode.FR)]
+		[DataRow("UK", null)]
+		public void Test_GetCountryCodeByCountryName(string countryName, CountryCode? expectedCountryCode)
+		{
+		}
+	}
+}
